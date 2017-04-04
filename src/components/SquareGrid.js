16d808a694c93822f3ca145b2859bc80c5f0990e
Sergio Flores <byoigres@gmail.com>
2017-04-04 14:48:43 -0600

    Added SVG of squares

--- src/components/SquareGrid.js
+++ src/components/SquareGrid.js
@@ -1,10 +1,21 @@
 import { h, render, Component } from 'preact';
 
 import styles from 'styles';
-
 const SquareGrid = () => (
   <div className={styles['square-grid']}>
-    
+    {/*Array(1444).fill().map((_,i) => <div data-key={`minute-${i+1}`} key={`minute-${i}`} className={styles['square-grid-square']} />)*/}
+    <svg width="100%" height="100%" viewBox="0,0,100%,100%" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <pattern id="smallGrid" width="1.388888888888889%" height="5%" patternUnits="userSpaceOnUse">
+                <path fill="#2e99e5" d="M0 0h960v960H0z" stroke="gray" stroke-width="0.5"/>
+            </pattern>
+            <pattern id="grid" width="8.333333333333333%" height="50%" patternUnits="userSpaceOnUse">
+                <rect width="100%" height="100%" fill="url(#smallGrid)"/>
+                <path fill="none" d="M0 0h960v960H0z" stroke="gray" stroke-width="2" />
+            </pattern>
+        </defs>
+        <rect width="100%" height="100%" fill="url(#grid)" />
+    </svg>
   </div>
 );
 

32e2847b2f53574e8cf7845f6b21188f9540225b
Sergio Flores <byoigres@gmail.com>
2017-04-10 18:37:28 -0600

    Added description in What is this section

--- src/containers/WhatIsThisContainer.js
+++ src/containers/WhatIsThisContainer.js
@@ -1,8 +1,15 @@
 import { h } from 'preact';
+import styles from 'styles';
 
 const WhatIsThis = () => (
   <div>
-    What is this?
+    <blockquote className={styles['blockquote-big']}>
+      We all have the exact same 1,440 minutes as Elon Musk, Bill Gates,
+      Mark Zuckerberg or whoever inspire you, you don't get more or less,
+      use it wisely.
+    </blockquote>
+    <h1>What is this?</h1>
+
   </div>
 );
 

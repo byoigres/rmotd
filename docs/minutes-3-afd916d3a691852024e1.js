a331a0c9144fff850c2013024f1cf7bfc84e9b04
Sergio Flores <byoigres@gmail.com>
2017-04-25 00:57:27 -0600

    Generated version that fix intl object working on firefox mobile

---
+++ docs/minutes-3-afd916d3a691852024e1.js
@@ -0,0 +1,1 @@
+webpackJsonp([3],{"./src/pages/About.js":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var s=n("./node_modules/react/react.js"),a=n.n(s),o=n("./node_modules/react-intl/lib/index.es.js"),r=function(){return a.a.createElement(o.FormattedHTMLMessage,{id:"content.about"})};t.default=r}});

5a12d23ef3f340153ce68b3f5ffbd7fdba8f4e14
Sergio Flores <byoigres@gmail.com>
2017-04-28 16:10:09 -0600

    Added GA

--- src/containers/ScrollToTop/ScrollToTop.js
+++ src/containers/ScrollToTop/ScrollToTop.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import styles from './styles';
+
+ReactGA.initialize('UA-98315944-1');
+
+const logPageView = ({ location }) => {
+  console.log('pathname', location.pathname);
+  // ReactGA.set({ page: location.pathname });
+  // ReactGA.pageview(location.pathname);
+};
 
 class ScrollToTop extends Component {
   componentDidUpdate(prevProps) {
@@ -10,6 +19,8 @@ class ScrollToTop extends Component {
   }
 
   render() {
+    this.props.history.listen(logPageView);
+
     return (
       <div className={styles['scroll-to-top']}>
         {this.props.children}
